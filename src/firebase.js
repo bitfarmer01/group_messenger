4583a78fd86748f6c6922831c7690e72af962155
Rajath <rajath37@gmail.com>
2021-07-27 11:40:59 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyA__hUc__djHr3z8A5SJZjP5QIyaZsYxYE",
+  authDomain: "messenger-14b08.firebaseapp.com",
+  projectId: "messenger-14b08",
+  storageBucket: "messenger-14b08.appspot.com",
+  messagingSenderId: "874640454464",
+  appId: "1:874640454464:web:7155be712b15d985ad80a5",
+};
+firebase.initializeApp(firebaseConfig);
