4d98e0873903fac38a608f2ee24678db4201b29b
Rajath <rajath37@gmail.com>
2021-08-03 11:33:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import styled from "styled-components";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import Chat from "./components/Chat";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import Login from "./components/Login";
+// import Spinner from 'react-spinkit';
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Messenger.svg"
+            alt=""
+          />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
+
   return (
-    <div className="App">
-      <h1>Hyello</h1>
+    <div className="app">
+      <Router>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <HeroSection>
+              <SideBar />
+              <Switch>
+                <Route path="/" exact>
+                  <Chat />
+                </Route>
+              </Switch>
+            </HeroSection>
+          </>
+        )}
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const HeroSection = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
